f8b6393d6d07e591445588c4f0b0bcaff96078e3
Michael Leßnau <michael.lessnau@gmail.com>
2017-04-10 21:50:15 +0200

    refactors configurations to separate files

--- gulpfile.js
+++ gulpfile.js
@@ -6,26 +6,20 @@ const ts = require('gulp-typescript');
 
 const tsProject = ts.createProject('./tsconfig.json');
 const targetDir = tsProject.config.compilerOptions.outDir;
+const mochaOptions = require('./mocha.json');
 
 gulp.task('clean', () => {
   return del(targetDir);
 });
 
 gulp.task('build', ['clean'], () => {
-  return tsProject.src()
-    .pipe(tsProject())
-    .js.pipe(gulp.dest(targetDir));
+  return tsProject.src().pipe(tsProject()).js.pipe(gulp.dest(targetDir));
 });
 
 gulp.task('test', () => {
-  return gulp.src('**/*.spec.ts')
-    .pipe(mocha({
-      'recursive': true,
-      'require': 'ts-node/register',
-      'reporter': 'dot',
-      'watch-extension': 'ts'
-    }));
+  return gulp.src('**/*.spec.ts').pipe(mocha(mochaOptions));
 });
 
 gulp.task('test:coverage', shell.task('./node_modules/.bin/nyc gulp test'));
+
 
