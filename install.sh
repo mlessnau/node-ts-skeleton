6d41150b5cc2713462a3d1502d1d7bb697566089
Michael Leßnau <michael.lessnau@gmail.com>
2017-04-15 10:28:29 +0200

    updates install.sh script

--- install.sh
+++ install.sh
@@ -7,11 +7,16 @@ mkdir -p $DST_DIR
 cp -r $SRC_DIR/src $DST_DIR/
 cp -r $SRC_DIR/test $DST_DIR/
 cp -r $SRC_DIR/.gitignore $DST_DIR/
-cp -r $SRC_DIR/mocha.opts $DST_DIR/
+cp -r $SRC_DIR/.nycrc $DST_DIR/
+cp -r $SRC_DIR/gulpfile.js $DST_DIR/
+cp -r $SRC_DIR/mocha.json $DST_DIR/
 cp -r $SRC_DIR/package.json $DST_DIR/
 cp -r $SRC_DIR/tsconfig.json $DST_DIR/
+cp -r $SRC_DIR/tslint.json $DST_DIR/
+
+touch $DST_DIR/README.md
 
 cd $DST_DIR
 git init .
 npm install
-npm test
+gulp test:spec
